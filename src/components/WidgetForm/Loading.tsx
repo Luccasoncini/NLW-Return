256efa67a7d69f645a07413d5d5676a6600fbd36
luccasoncini <luccadiassoncini@gmail.com>
2022-05-07 12:22:41 -0300

    terceiro dia de aula

--- src/components/WidgetForm/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { CircleNotch } from "phosphor-react";
-
-export function Loading(){
-    return (
-        <div className="w-6 h-6 flex justify-center items-center overflow-hidden">
-            <CircleNotch weight="bold" className="h-4 w-4 animate-spin"/>
-        </div>
-    )
-}
